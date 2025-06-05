7c52c8a3ddd66d09b0724f67bd9195aae70e84e2
AV <110290251+traevelliath@users.noreply.github.com>
2025-06-05 14:29:04 +0500

    Update project.lua
    change deprecated buf_get_clients() to get_clients()

--- lua/project_nvim/project.lua
+++ lua/project_nvim/project.lua
@@ -13,7 +13,7 @@ function M.find_lsp_root()
   -- Get lsp client for current buffer
   -- Returns nil or string
   local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-  local clients = vim.lsp.buf_get_clients()
+  local clients = vim.lsp.get_clients()
   if next(clients) == nil then
     return nil
   end
